c0a88a0855cbef30a94cd611319eb895f230040f
Jesus M. Castagnetto <jesus@castagnetto.com>
2022-01-15 17:16:24 -0500

    refactor code

--- 02-plots.R
+++ 02-plots.R
@@ -4,114 +4,69 @@ library(patchwork)
 
 ipress <- readRDS("extra/ipress.rds")
 
-antig <- readRDS("20220115/antig.rds") %>%
-  filter(!is.na(precio) &
-           precio > 0.02) %>% # eliminar valores cercanos a cero
-  left_join(
-    ipress,
-    by = c("codigo_ipress" = "COD_IPRESS")
-  ) %>%
-  group_by(DEPARTAMENTO) %>%
-  summarise(
-    n = n(),
-    n_distinct = n_distinct(codigo_ipress),
-    min = min(precio, na.rm = TRUE),
-    max = max(precio, na.rm = TRUE),
-    sd = sd(precio, na.rm = TRUE),
-    med = median(precio, na.rm = TRUE),
-    mean = mean(precio, na.rm = TRUE)
-  ) %>%
-  ungroup() %>%
-  mutate(
-    dpto_lbl = paste0(DEPARTAMENTO, " (N: ", n, ")"),
-    dpto_lbl = fct_reorder(dpto_lbl, mean, .desc = FALSE)
-  )
+load_data <- function(rdsfile, ipress) {
+  readRDS(rdsfile) %>%
+    filter(!is.na(precio) &
+             precio > 0.02) %>% # eliminar valores cercanos a cero
+    left_join(
+      ipress,
+      by = c("codigo_ipress" = "COD_IPRESS")
+    ) %>%
+    group_by(DEPARTAMENTO) %>%
+    summarise(
+      n = n(),
+      n_distinct = n_distinct(codigo_ipress),
+      min = min(precio, na.rm = TRUE),
+      max = max(precio, na.rm = TRUE),
+      sd = sd(precio, na.rm = TRUE),
+      med = median(precio, na.rm = TRUE),
+      mean = mean(precio, na.rm = TRUE)
+    ) %>%
+    ungroup() %>%
+    mutate(
+      dpto_lbl = paste0(DEPARTAMENTO, " (N: ", n, ")"),
+      dpto_lbl = fct_reorder(dpto_lbl, mean, .desc = FALSE)
+    )
+}
 
-pcr <- readRDS("20220115/pcr.rds") %>%
-  filter(!is.na(precio) &
-           precio > 0.02) %>% # eliminar valores cercanos a cero
-  left_join(
-    ipress,
-    by = c("codigo_ipress" = "COD_IPRESS")
-  ) %>%
-  group_by(DEPARTAMENTO) %>%
-  summarise(
-    n = n(),
-    n_distinct = n_distinct(codigo_ipress),
-    min = min(precio, na.rm = TRUE),
-    max = max(precio, na.rm = TRUE),
-    sd = sd(precio, na.rm = TRUE),
-    med = median(precio, na.rm = TRUE),
-    mean = mean(precio, na.rm = TRUE)
-  ) %>%
-  ungroup() %>%
-  mutate(
-    dpto_lbl = paste0(DEPARTAMENTO, " (N: ", n, ")"),
-    dpto_lbl = fct_reorder(dpto_lbl, mean, .desc = FALSE)
-  )
+antig <- load_data("20220115/antig.rds", ipress)
+pcr <- load_data("20220115/pcr.rds", ipress)
 
-pcr_plot <- ggplot(
-  pcr %>%
-    filter(!is.na(DEPARTAMENTO)),
-  aes(x = mean, y = dpto_lbl)
-) +
-  geom_errorbarh(
-    aes(xmin = min, xmax = max),
-    color = "grey40",
-    na.rm = TRUE,
-    size = 1,
-    height = .5
+mk_plot <- function(df, title_lbl) {
+  ggplot(
+    df %>%
+      filter(!is.na(DEPARTAMENTO)) %>%
+      filter(mean > 0),
+    aes(x = mean, y = dpto_lbl)
   ) +
-  scale_x_continuous(
-    labels = scales::label_dollar(
-      prefix = "S/ ",
-      accuracy = .01),
-    n.breaks = 6
-  ) +
-  geom_text_repel(
-    aes(label = sprintf("%.2f", mean)),
-    nudge_y = .5
-  ) +
-  geom_point(size = 4, color = "red") +
-  theme_bw(15) +
-  labs(
-    title = "Pruebas PCR",
-    x = "",
-    y = ""
-  )
-#pcr_plot
+    geom_errorbarh(
+      aes(xmin = min, xmax = max),
+      color = "grey40",
+      na.rm = TRUE,
+      size = 1,
+      height = .5
+    ) +
+    geom_text_repel(
+      aes(label = sprintf("%.2f", mean)),
+      nudge_y = .5
+    ) +
+    geom_point(size = 4, color = "brown") +
+    scale_x_continuous(
+      labels = scales::label_dollar(
+        prefix = "S/ ",
+        accuracy = .01),
+      n.breaks = 6
+    ) +
+    theme_bw(15) +
+    labs(
+      title = title_lbl,
+      x = "",
+      y = ""
+    )
+}
 
-antig_plot <- ggplot(
-  antig %>%
-    filter(!is.na(DEPARTAMENTO)) %>%
-    filter(mean > 0),
-  aes(x = mean, y = dpto_lbl)
-) +
-  geom_errorbarh(
-    aes(xmin = min, xmax = max),
-    color = "grey40",
-    na.rm = TRUE,
-    size = 1,
-    height = .5
-  ) +
-  geom_text_repel(
-    aes(label = sprintf("%.2f", mean)),
-    nudge_y = .5
-  ) +
-  geom_point(size = 4, color = "brown") +
-  scale_x_continuous(
-    labels = scales::label_dollar(
-      prefix = "S/ ",
-      accuracy = .01),
-    n.breaks = 6
-  ) +
-  theme_bw(15) +
-  labs(
-    title = "Pruebas antigénicas",
-    x = "",
-    y = ""
-  )
-#antig_plot
+pcr_plot <- mk_plot(pcr, "Pruebas PCR")
+antig_plot <- mk_plot(antig, "Pruebas antigénicas")
 
 comb_plot <- (pcr_plot + antig_plot) +
   plot_annotation(
